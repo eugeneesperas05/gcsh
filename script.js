d610fc2b940549daec4b171442f1f4fe9d71019e
Eugene Esperas <eugene.esperas05@gmail.com>
2025-05-08 22:23:40 +0800

    hidden buttons added

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // ----GCSH
+let body = document.querySelector("body");
 
 let btnHome = document.querySelector(".btn-home");
 btnHome.addEventListener("click", () => {
-  console.log("hello");
+  body.style.backgroundImage = "url('asssets/2.png')";
 });
 
 let btnTransactions = document.querySelector(".btn-transactions");
 btnTransactions.addEventListener("click", () => {
-  console.log("hello");
+  body.style.backgroundImage = "url('asssets/1.png')";
 });
 
 let btnProfile = document.querySelector(".btn-profile");
 btnProfile.addEventListener("click", () => {
-  console.log("hello");
+  body.style.backgroundImage = "url('asssets/3.png')";
 });
