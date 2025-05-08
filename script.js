5670e2cc7342cd38f4d9b299c2db0111a1a946ba
Eugene Esperas <eugene.esperas05@gmail.com>
2025-05-08 21:59:51 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// ----GCSH
+
+let btnHome = document.querySelector(".btn-home");
+btnHome.addEventListener("click", () => {
+  console.log("hello");
+});
+
+let btnTransactions = document.querySelector(".btn-transactions");
+btnTransactions.addEventListener("click", () => {
+  console.log("hello");
+});
+
+let btnProfile = document.querySelector(".btn-profile");
+btnProfile.addEventListener("click", () => {
+  console.log("hello");
+});
